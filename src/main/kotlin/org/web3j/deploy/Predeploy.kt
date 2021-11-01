36fdf8e38fc6e770388b0bd059ff1d39a1e1b7d7
Christian Felde <cfelde@cfelde.com>
2021-11-01 12:36:56 +0000

    Remove all things not related to plugin

--- src/main/kotlin/org/web3j/deploy/Predeploy.kt
+++
@@ -1,5 +0,0 @@
-package org.web3j.deploy
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
-annotation class Predeploy(val profile: String)
